52329e67d1a128af68c4302f28364fbd61efc0df
IA-OCSB <97174166+IA-OCSB@users.noreply.github.com>
2022-01-21 13:23:56 +0000

    hi

---
+++ UNIT 4/U4A4/Q1Counting/Q1.py
@@ -0,0 +1,20 @@
+#asking for large number and number to count by 
+print('Please enter a large number...')
+
+#have an input so that the user may insert a large number and number to count by. then give the values a variable 
+number = int(input())
+print("Please enter a number to count by...")
+count = int(input())
+counter = 0
+
+
+if number != counter:
+  print(counter)
+  counter = counter + count
+    
+    
+else: 
+  print(counter)
+  counter = counter - count
+  if counter == 0:
+    print("Finished. Wow that took a long time and it is completely useless. ")
